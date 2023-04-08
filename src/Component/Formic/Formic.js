5ae60702e591d857731f05f83ae7e924f69e6a0d
usamafaheemAhmed <117355964+usamafaheemAhmed@users.noreply.github.com>
2023-04-08 15:27:29 +0500

    Formic Completed on 8_Apr_2023

--- src/Component/Formic/Formic.js
+++ src/Component/Formic/Formic.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
+import { Button, Col, Modal, Row } from 'react-bootstrap'
 import "./formic.css"
 import { Slider } from '@mui/material'
-import { useState } from 'react'
-import { useRef } from 'react'
-import { useEffect } from 'react'
+import { useState , useRef , useEffect } from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 
 
 
 const Formic = () => {
-  let [range1 , setRange1]= useState(100000);
-  let [range2 , setRange2]= useState(70000);
-  let [range3 , setRange3]= useState(3500);
+
+  const [show, setShow] = useState(false);
+  const [result, setResult] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true)
 
   const ref = useRef(700);
   useEffect(() => {
@@ -40,87 +42,209 @@ const Formic = () => {
           your own home may be closer than <br/>
           you think.
           </p>
-          <Button size="lg" variant='outline-success'>Get complete report</Button>
+          <Button size="lg" variant='outline-success' onClick={handleShow}>Get complete report</Button>
           <p>Yes, it's really free and we do not need your
           name and email address to show your results
           </p>
+
+          <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login form</Modal.Title>
+          </Modal.Header>
+          <Formik
+          initialValues={{email:'',password:'' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = '*Email is required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = '*Invalid email address';
+            }
+            if (!values.password) {
+              errors.password = '*Password is required';
+            } else if (
+              !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/i.test(values.password)
+            ) {
+              errors.password = '*Password must contain Capital letter,Number and Special character ';
+            }
+
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            handleClose();
+            localStorage.setItem("logedin",1);
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+            //   setSubmitting(false);
+            // }, 400);
+          }}>
+          {({isSubmitting})=>{
+            return  (
+              <Form>
+          <Modal.Body>
+                <label className="my-2" for="email">Email:</label>
+                <Field type="text" id="email" name="email" className="form-control firstInput" />
+                <ErrorMessage  name="email" component="div"/>
+                <label className="my-2" for="password">Password:</label>
+                <Field type="text" id="password" name="password" className="form-control firstInput" />
+                <ErrorMessage  name="password" component="div"/>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button className="close" variant="outline-light" onClick={handleClose}>
+              Close
+            </Button>
+            <Button className="login" variant="outline-success" type='submit' disabled={isSubmitting}>
+              Login
+            </Button>
+          </Modal.Footer>
+          </Form>)}}
+          </Formik>
+        </Modal>
+      
+            
+            
+
+
+
           </Col>
           <Col md="7">
           <div className="card text-start p-3 ">
             <div className="card-body">
-            <div className="row">
-                  <div className="col-md-12">
-                      <div className="row">
-                          <div className="col-md-9 pt-2">
-                              <label for="input1">
-                              Yearly income before taxes
-                              </label>
-                          </div>
-                          <div className="col-md-3">
-                            <div className='input-group'>
-                            <input type='number' id="input1" className='form-control firstInput' placeholder="$ 00" value={range1} onChange={(e)=>{setRange1(e.target.value)}} />
-                            <span class="input-group-text append">CHF</span>
-                            </div> 
-                          </div>
-                      </div>
-                  </div>
-                  <div className="col-md-12 mt-2">
-                      <Slider aria-label="Temperature" step={1} min={10000} max={500000} value={range1} onChange={(e)=>{setRange1(e.target.value)}}  defaultValue={100000} />
-                  </div>
-                  <div className="col-md-12" id="limit">
-                      <p>10,000 CHF <span className="float-end">500,000 CHF</span></p>
-                  </div>
-              </div>
-            <div className="row">
-              <div className="col-md-12">
-                  <div className="row">
-                      <div className="col-md-9 pt-2">
-                          <label for="input2">
-                          Savings available for a down payment
-                          </label>
-                      </div>
-                      <div className="col-md-3">
-                        <div className='input-group'>
-                        <input type='number' id="input2" className='form-control firstInput' placeholder="$ 00" value={range2} onChange={(e)=>{setRange2(e.target.value)}} />
-                        <span class="input-group-text append">CHF</span>
-                        </div> 
-                      </div>
-                  </div>
-              </div>
-              <div className="col-md-12 mt-2">
-                  <Slider aria-label="Temperature" step={1} min={10000} max={700000} value={range2} onChange={(e)=>{setRange2(e.target.value)}}  defaultValue={100000} />
-              </div>
-              <div className="col-md-12" id="limit">
-                  <p>10,000 CHF <span className="float-end">700,000 CHF</span></p>
-              </div>
-              </div>
+            <Formik
+            initialValues={{range1:100000, range2 :70000, range3 :3500 }}
+            validate={(values) => {
+              const errors = {};
+              if (values.range1 >= values.range2) {
+                errors.range2 = "Yearly income before taxes must be Greater then Savings available for a down payment";
+              }
+              return errors;
+            }}
+            onSubmit={(values) => {
+              let login = localStorage.getItem("logedin");
+              if(!login){
+                handleShow();
+              }
+              else{
+                // Max affordable home price = (Yearly income before taxes - (Current rent x 12)) * Affordability factor + Savings available for a down payment
+                let yearlyIncome = values.range1;
+                let savingAvailable = values.range2;
+                let currentRent = values.range3;
+
+                let maxAffordable = (yearlyIncome - (currentRent * 12)) * 0.1792 + savingAvailable;
+
+                setResult(Math.round(maxAffordable));
+
+              }
+      
+            }}>
+
+            {({values, isSubmitting, handleChange})=>{
+              if(!result){
+                return  (
+                  <Form className="slide-in-left">  
+                    <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-9 pt-2">
+                                <label for="input1">
+                                Yearly income before taxes
+                                </label>
+                            </div>
+                            <div className="col-md-3">
+                              <div className='input-group'>
+                              <Field type='number' name='range1' className='form-control firstInput' placeholder="$ 00"  />
+                              <span class="input-group-text append">CHF</span>
+                              </div> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-12 mt-2">
+                        <Slider aria-label="Temperature" name='range1'  step={1} min={10000} max={500000} value={values.range1} onChange={handleChange} defaultValue={100000} />
+                    </div>
+                    <div className="col-md-12" id="limit">
+                        <p>10,000 CHF <span className="float-end"> 500,000 CHF</span></p>
+                    </div>
+                    <ErrorMessage className="text-center" name="range1" component="div"/>
+                </div>
               <div className="row">
                 <div className="col-md-12">
                     <div className="row">
                         <div className="col-md-9 pt-2">
-                            <label for="input3">
-                            Current rent
+                            <label for="input2">
+                            Savings available for a down payment
                             </label>
                         </div>
                         <div className="col-md-3">
                           <div className='input-group'>
-                          <input type='number' id="input3" className='form-control firstInput' placeholder="$ 00" value={range3} onChange={(e)=>{setRange3(e.target.value)}} />
+                          <Field type='number' id="input2" name="range2" className='form-control firstInput' placeholder="$ 00"  />
                           <span class="input-group-text append">CHF</span>
                           </div> 
                         </div>
                     </div>
                 </div>
                 <div className="col-md-12 mt-2">
-                    <Slider aria-label="Temperature" step={1} min={500} max={7000} value={range3} onChange={(e)=>{setRange3(e.target.value)}} defaultValue={100000} />
+                    <Slider aria-label="Temperature" name="range2" step={1} min={10000} max={700000} value={values.range2} onChange={handleChange}   defaultValue={100000} />
                 </div>
                 <div className="col-md-12" id="limit">
-                    <p>500 CHF <span className="float-end">7,000 CHF</span></p>
+                    <p>10,000 CHF <span className="float-end">700,000 CHF</span></p>
                 </div>
-              </div>
-              <div className='col-md-12 text-center'>
+                <ErrorMessage className="text-center" name="range2" component="div"/>
+                </div>
+                <div className="row">
+                  <div className="col-md-12">
+                      <div className="row">
+                          <div className="col-md-9 pt-2">
+                              <label for="input3">
+                              Current rent
+                              </label>
+                          </div>
+                          <div className="col-md-3">
+                            <div className='input-group'>
+                            <Field type='number' id="input3" name="range3" className='form-control firstInput' placeholder="$ 00"  />
+                            <span class="input-group-text append">CHF</span>
+                            </div> 
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-md-12 mt-2">
+                      <Slider aria-label="Temperature" name="range3"  step={1} min={500} max={7000} value={values.range3} onChange={handleChange} defaultValue={100000} />
+                  </div>
+                  <div className="col-md-12" id="limit">
+                      <p>500 CHF <span className="float-end">7,000 CHF</span></p>
+                  </div>
+                  <ErrorMessage className="text-center" name="range3" component="div"/>
+                </div>
+                <div className='col-md-12 text-center'>
+  
+                  <button className='btn btn-outline-light w-25 CheckBtn' type="submit">Check now</button>
+                </div>
+                </Form>
+                )
+              }
+              else {
+                return(
+                  <div className='resultDiv slide-in-left'>
 
-                <button className='btn btn-outline-light w-25 CheckBtn'>Check now</button>
-              </div>
+                    <span className='text-center'>You can afford {result} amount</span>
+                  
+                    <button className='btn btn-outline-light w-25 CheckBtn' onClick={()=>{setResult()}}>Calculate again</button>
+
+                  </div>
+                );
+
+              }
+
+              
+              }
+          
+              }
+        </Formik>
+
+
+
+
             </div>
           </div>
           </Col>
---
+++ src/Component/Formic/readme.md
@@ -0,0 +1,4 @@
+#Formic Ui
+
+
+![image](https://user-images.githubusercontent.com/117355964/230657600-1163252b-b16f-4f4f-9192-0af8f924abc4.png)
